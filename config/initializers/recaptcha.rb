8cd21cea088415f7ccd9e38e836435e6fe76ac8c
Eugene Peluhnya <peluhnya@outlook.com>
2020-07-30 10:36:32 +0300

    add

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,6 @@
+Recaptcha.configure do |config|
+  config.site_key = '6LfxqqEUAAAAAAfyxsnI73poWVK8GVv53Pz7nfhu'
+  config.secret_key = '6LfxqqEUAAAAADJwWgjcoI-O_-dEtdoeQHK7kvaZ'
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+end
